fa2f2728185d4b86a5eb96b4011100d02105c562
deepanshu123412 <83260770+deepanshu123412@users.noreply.github.com>
2025-04-26 17:10:58 +0530

    Update SampleTest.java
    someneeded changes

--- SampleTest.java
+++ SampleTest.java
@@ -1,16 +1,50 @@
 package test;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class SampleTest {
+
+    WebDriver driver;
+    ExtentReports extent;
+    ExtentTest test;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        test = extent.createTest("Google Search Test", "Sample test to open Google");
+    }
+
     @Test
     public void testGoogle() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        System.out.println("Title is: " + driver.getTitle());
+        String title = driver.getTitle();
+        System.out.println("Title is: " + title);
+
+        if (title.contains("Google")) {
+            test.pass("Navigated to Google successfully!");
+        } else {
+            test.fail("Google title not found!");
+        }
+    }
+
+    @After
+    public void tearDown() {
         driver.quit();
+        extent.flush();
     }
 }
+
